72a807a9457da8c08cca4f0d3dc4ed03be0910a4
fedluc <flc@kth.se>
2020-12-16 19:38:53 +0100

    Removed python folder

--- python/press_average.py
+++
@@ -1,9 +0,0 @@
-#!/home/x_fedlu/myPython/bin/python2.7 -u
-
-import hsmc_pressure as hsp
-
-hsp.pressure_virial(data_dir="../../HSMC_tests/nvt_simulation_1",samples_block=2000)
-hsp.pressure_thermo(data_dir="../../HSMC_tests/nvt_simulation_1",samples_block=2000)
-hsp.pressure_virial(data_dir="../../HSMC_tests/nvt_simulation_2",samples_block=2000)
-hsp.pressure_thermo(data_dir="../../HSMC_tests/nvt_simulation_2",samples_block=2000)
-# hsp.pressure_thermo(data_dir="../tests",samples_block=2000,npt=True)
